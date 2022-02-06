3a54d895c149f5955b9a6e078994c8a91f4eb022
fredriclesomar <fredric.sjl@gmail.com>
2022-02-06 13:39:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 	// create 1 file -> ip.log
 	$file = fopen ('ip.log', 'a+');
 	fwrite($file, date('Y-m-d H:i:s') . " - $user_ip" . " - $agent \r\n")
+
+
+	$user_ip = $_SERVER['REMOTE_ADDR'];
+	$agent = $_SERVER['HTTP_USER_AGENT'];
